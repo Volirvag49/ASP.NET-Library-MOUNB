2904875331c51b909f5572e4f344405a5a5ed962
Volirvag49 <lirikvolirvag@gmail.com>
2017-09-14 02:53:20 +1100

    Добавлено больше пользователей

--- MOUNB/Models/Context/SampleData.cs
+++ MOUNB/Models/Context/SampleData.cs
@@ -12,7 +12,27 @@ namespace MOUNB.Models
             // Добавление пользователей
             List<User> users = new List<User>
             {
-                new User{Name="Пользователь по умолчанию", Login="111111", Password="111111", Position="Администратор", Role = UserRole.Администратор }
+                new User{Name="Пользователь по умолчанию", Login="111111", Password="111111", Position="Администратор", Role = UserRole.Администратор },
+                new User{Name="аа", Login="202020", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="бб", Login="191919", Password="111111", Position="Тест", Role = UserRole.Администратор },
+                new User{Name="вв", Login="181818", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="гг", Login="171717", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="дд", Login="161616", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="ее", Login="151515", Password="111111", Position="Тест", Role = UserRole.Администратор },
+                new User{Name="ёё", Login="141414", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="жж", Login="131313", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="зз", Login="121212", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="ии", Login="111111", Password="111111", Position="Тест", Role = UserRole.Администратор },
+                new User{Name="кк", Login="101010", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="лл", Login="999", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="мм", Login="888", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="нн", Login="777", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="оо", Login="666", Password="111111", Position="Тест", Role = UserRole.Администратор },
+                new User{Name="пп", Login="555", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="рр", Login="444", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="сс", Login="333", Password="111111", Position="Тест", Role = UserRole.Администратор },
+                new User{Name="тт", Login="222", Password="111111", Position="Тест", Role = UserRole.Пользователь },
+                new User{Name="уу", Login="111", Password="111111", Position="Тест", Role = UserRole.Пользователь },
             };
 
             foreach (User user in users)
